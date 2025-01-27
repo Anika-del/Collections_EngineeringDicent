f00b49804ea270179186c3104e94873e2203cfd1
Anika Patel <76812213+Anika-del@users.noreply.github.com>
2025-01-27 19:21:42 +0530

    Collection code by EngineeringDicent Sir

---
+++ ArrayList1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayList1{
+
+    public static void main(String[] args) {
+        // Create an ArrayList
+       // ArrayList<String> list = new ArrayList<>();
+        ArrayList<Integer> arraylist = new ArrayList<>();
+       arraylist.add(1);
+       arraylist.add(2);
+       arraylist.add(3);
+       System.out.println(arraylist);
+        }
+}
